401da012a58e38787034940428b81f751e8d5346
Rodrigo Trancoso <trancosorodrigo1@gmail.com>
2025-02-13 19:54:01 -0300

    feat: Add tests and ci pipeline

--- src/controllers/order.controller.js
+++ src/controllers/order.controller.js
@@ -1,41 +1,58 @@
 import OrderService from "../services/order.service.js";
 
-export const createOrder = async (req, res) => {
-  try {
-    const { customerName, items } = req.body;
-    const order = await OrderService.createOrder(customerName, items);
-    res.status(201).json(order);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-};
-
-export const getOrders = async (req, res) => {
-  
+const OrderController = {
+  createOrder: async (req, res) => {
     try {
-    const orders = await OrderService.getOrders();
-    res.json(orders);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
+      const { customerId, items } = req.body;
+
+      if (!items) return res.status(400).json({ message: "Items não informados" });
+      for (let item of items) {
+        if (!item.productId || !item.productName || !item.quantity || !item.price) {
+          return res.status(400).json({ message: "Item inválido" });
+        }
+      }
+
+      const order = await OrderService.createOrder(customerId, items);
+      res.status(201).json(order);
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  },
+
+  getOrders: async (req, res) => {
+    try {
+      const orders = await OrderService.getOrders();
+      res.status(200).json(orders);
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  },
+
+  getOrderById: async (req, res) => {
+    try {
+      const order = await OrderService.getOrderById(req.params.id);
+      if (!order) return res.status(404).json({ message: "Pedido não encontrado" });
+      res.status(200).json(order);
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  },
+
+  updateOrderStatus: async (req, res) => {
+    try {
+      const { status } = req.body;
+
+      if (!status) return res.status(400).json({ message: "Status não informado" });
+
+      const order = await OrderService.getOrderById(req.params.id);
+      if (!order) return res.status(404).json({ message: "Pedido não encontrado" });
+
+      const updatedOrder = await OrderService.updateOrderStatus(req.params.id, status);
+      res.status(200).json(updatedOrder);
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
   }
 };
 
-export const getOrderById = async (req, res) => {
-  try {
-    const order = await OrderService.getOrderById(req.params.id);
-    if (!order) return res.status(404).json({ message: "Pedido não encontrado" });
-    res.json(order);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-};
-
-export const updateOrderStatus = async (req, res) => {
-  try {
-    const { status } = req.body;
-    const order = await OrderService.updateOrderStatus(req.params.id, status);
-    res.json(order);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-};
+export default OrderController;
--- src/models/order.model.js
+++ src/models/order.model.js
@@ -1,13 +1,24 @@
-import mongoose  from "mongoose";
+import mongoose from 'mongoose';
 
 const OrderSchema = new mongoose.Schema(
-    {}, { collection: 'Order' }
+    {
+        customerId: { type: String, required: false },
+        items: [
+            {
+                productId: { type: String, required: true },
+                productName: { type: String, required: true },
+                quantity: { type: Number, required: true },
+                price: { type: Number, required: true }
+            },
+        ],
+        totalPrice: { type: Number, required: true },
+        status: { type: String, required: true, default: "PENDING" },
+        createdAt: { type: Date, required: true, default: Date.now },
+        updatedAt: { type: Date, required: true, default: Date.now }
+    }, { collection: 'Order' }
 );
 
 
 const Order = mongoose.model("Order", OrderSchema);
 
 export default Order;
-
-//testet
-//lalal dasdsad
