2ce6262daa0338b4d3fdfde75248a6d4ed761844
RaildoBru <raildobruno@gmail.com>
2025-02-18 01:01:09 -0300

    Adicionando documentação

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./db-config.js";
 import orderRoutes from "./src/routes/order.routes.js";
+import swaggerUi from "swagger-ui-express";
+import swaggerSpec from "./src/docs/swagger.js";
 
 dotenv.config();
 const app = express();
@@ -11,6 +13,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/orders", orderRoutes);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 const PORT = process.env.PORT || 5000;
 
